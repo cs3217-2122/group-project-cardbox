fde2f35a4e25a7da936f9255b4075ee1a5d74b2a
Bernard Wan De Yuan <bernardwan99@gmail.com>
2022-03-07 17:10:59 +0800

    Initial Commit

---
+++ CardBox/CardBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardBoxApp.swift
+//  CardBox
+//
+//  Created by Bernard Wan on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CardBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
