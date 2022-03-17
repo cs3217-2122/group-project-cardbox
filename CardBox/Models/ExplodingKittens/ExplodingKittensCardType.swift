d3a3b83058d928f512a787ffb36b12dfea712ed2
Bryann Yeap Kok Keong <bryannyeapkk@gmail.com>
2022-03-17 19:14:58 +0800

    Combine exploding kitten enums

--- CardBox/Models/ExplodingKittens/ExplodingKittensCardType.swift
+++ CardBox/Models/ExplodingKittens/ExplodingKittensCardType.swift
@@ -10,10 +10,32 @@ enum ExplodingKittensCardType: String {
     case bomb = "bomb"
     case defuse = "defuse"
     case favor = "favor"
+    case nope = "nope"
     case seeTheFuture = "see-the-future"
     case shuffle = "shuffle"
     case skip = "skip"
     case random1 = "random-1"
     case random2 = "random-2"
     case random3 = "random-3"
+
+    var initialFrequency: Int {
+        switch self {
+        case .attack:
+            return 4
+        case .favor:
+            return 4
+        case .nope:
+            return 5
+        case .seeTheFuture:
+            return 5
+        case .shuffle:
+            return 4
+        case .skip:
+            return 4
+        case .random1, .random2, .random3:
+            return 4
+        case .bomb, .defuse:
+            return 0
+        }
+    }
 }
