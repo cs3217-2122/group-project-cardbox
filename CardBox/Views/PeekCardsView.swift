42c3673b054b316c1e0042b86c3cb8e22f3c31f2
bernardwan <bernardwan99@gmail.com>
2022-03-19 14:38:28 +0800

    Add PeekCardsView

---
+++ CardBox/Views/PeekCardsView.swift
@@ -0,0 +1,20 @@
+//
+//  PeekCardsView.swift
+//  CardBox
+//
+//  Created by Bernard Wan on 19/3/22.
+//
+
+import SwiftUI
+
+struct PeekCardsView: View {
+    var cards: [Card]
+
+    var body: some View {
+        HStack {
+            ForEach(cards) { card in
+                CardView(cardViewModel: CardViewModel(card: card, isFaceUp: true))
+            }
+        }
+    }
+}
