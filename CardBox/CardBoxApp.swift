83026a8b67aabca3dd4efa2bc7ebf7b406197169
Stuart Long <chayboon@gmail.com>
2022-03-28 23:15:21 +0800

    Fix formatting

--- CardBox/CardBoxApp.swift
+++ CardBox/CardBoxApp.swift
@@ -10,9 +10,11 @@ import Firebase
 
 @main
 struct CardBoxApp: App {
+
     init() {
         FirebaseApp.configure()
     }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
