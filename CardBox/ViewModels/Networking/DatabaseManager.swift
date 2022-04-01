75aca7faced76be3adcfa11f4a2e604d48efa3d2
Stuart Long <chayboon@gmail.com>
2022-04-01 12:36:26 +0800

    Create database manager class

---
+++ CardBox/ViewModels/Networking/DatabaseManager.swift
@@ -0,0 +1,10 @@
+//
+//  DatabaseManager.swift
+//  CardBox
+//
+//  Created by Stuart Long on 1/4/22.
+//
+class DatabaseManager {
+    // This class is used to propogate changes of the gamestate to the realtime database
+
+}
