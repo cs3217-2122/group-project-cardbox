b67b3898a9f9eda59bae8ad690e04dd803a6a67d
bernardwan <bernardwan99@gmail.com>
2022-04-17 07:50:53 +0800

    Add property same colour check

--- CardBox/MonopolyDeal/Models/MonopolyDealPropertySet.swift
+++ CardBox/MonopolyDeal/Models/MonopolyDealPropertySet.swift
@@ -6,7 +6,23 @@
 //
 
 class MonopolyDealPropertySet: MonopolyDealCardCollection {
+    var setColour: PropertyColor
+
+    init(setColour: PropertyColor) {
+        self.setColour = setColour
+        super.init()
+    }
+
+    // TODO: Implement
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
+
     override func canAdd(_ card: Card) -> Bool {
-        card is PropertyCard
+        if let card = card as? PropertyCard {
+            return card.colors.contains(setColour)
+        } else {
+            return false
+        }
     }
 }
